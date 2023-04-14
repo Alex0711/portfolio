660f78f57c2dc05199414b9b007cac6edb1c55cf
Alex0711 <alex.senger@hotmail.com>
2023-04-14 00:35:57 -0300

    add favicon and meta

--- app/head.js
+++ app/head.js
@@ -1,7 +1,11 @@
 export default function Head() {
   return (
     <>
+      <meta name="description" content="The Portfolio of a passionate full stack developer specialized in backend" />
+      <meta name="robots" content="index,follow" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <title>My Portfolio </title>
+      <link rel="icon" href="portfolio.ico" />
     </>
   );
 }
